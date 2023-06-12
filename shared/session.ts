e542a96ff21baf709c98cac37e9430f03f789824
kasperstorgaard <kasper.storgaard@gmail.com>
2023-06-12 16:28:42 +0200

    add recent gigs to session

--- shared/session.ts
+++ shared/session.ts
@@ -1,22 +1,61 @@
 import { Session } from "fresh_session";
 import { Group } from "./db/groups.ts";
-
-export function updateRecentGroups(session: Session, group: Group) {
-  const recentGroups = getRecentGroups(session)
-    .filter(item => item.id !== group.id);
-  recentGroups.unshift(group);
-
-  session.set("recent-groups", JSON.stringify(recentGroups));
-}
+import { Gig } from "./db/gigs.ts";
 
 export function getRecentGroups(session: Session) {
   const rawValue = session.get("recent-groups");
 
-  let recentGroups: Group[] = [];
+  let recentGroups: Pick<Group, "id" | "name" | "slug">[] = [];
 
   try {
     recentGroups = JSON.parse(rawValue);
-  } catch (_err) { /** */ }
+  } catch (_err) {
+    /** */
+  }
 
   return recentGroups;
+}
+
+export function updateRecentGroups(session: Session, group: Group) {
+  const recentGroups = getRecentGroups(session).filter(
+    (item) => item.id !== group.id
+  );
+
+  recentGroups.unshift({
+    id: group.id,
+    name: group.name,
+    slug: group.slug,
+  });
+
+  session.set("recent-groups", JSON.stringify(recentGroups));
+}
+
+export function getRatedGigs(session: Session) {
+  const rawValue = session.get("rated-gigs");
+
+  let ratedGigs: Pick<Gig, "id" | "name" | "slug">[] = [];
+
+  try {
+    ratedGigs = JSON.parse(rawValue);
+  } catch (_err) {
+    ratedGigs = [];
+  }
+
+  console.log({ ratedGigs });
+
+  return ratedGigs;
+}
+
+export function updateRatedGigs(session: Session, gig: Gig) {
+  const ratedGigs = getRatedGigs(session).filter((item) => item.id !== gig.id);
+
+  ratedGigs.unshift({
+    id: gig.id,
+    name: gig.name,
+    slug: gig.slug,
+  });
+
+  console.log("after", { ratedGigs });
+
+  session.set("rated-gigs", JSON.stringify(ratedGigs));
 }
