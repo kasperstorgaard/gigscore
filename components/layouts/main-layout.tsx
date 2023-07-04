73ea7f4993c336787d5c3d6298d83768986f1e55
kasperstorgaard <kasper.storgaard@gmail.com>
2023-07-04 22:14:47 +0200

    change font family

--- components/layouts/main-layout.tsx
+++ components/layouts/main-layout.tsx
@@ -16,10 +16,9 @@ export default function MainLayout({ children }: Props) {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossOrigin" />
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200&display=swap" rel="stylesheet" />
 
         <link rel="stylesheet" href={asset("/base/main.css")} />
         <link rel="stylesheet" href={asset("/layouts/main-layout.css")} />
